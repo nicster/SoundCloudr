a8a7151e75ab588540a534860fa4309638b1fa29
Nicolas Spycher <nic.spycher@gmail.com>
2014-09-26 23:33:20 +0200

    Added command functionality, especially help and exit command

--- soundcloudr/__init__.py
+++ soundcloudr/__init__.py
@@ -7,32 +7,72 @@
 # http://www.wtfpl.net/ for more details.
 
 import sys
-import soundcloud
+import json
+
+import requests
 
 TOKEN = '1-97489-6257437-80f8de20636a16b3'
+API_BASE = 'https://api.soundcloud.com'
 
-def help_():
-    print 'This is the help page'
+class Playlist():
+    def __init__(self):
+        r = requests.get(
+            API_BASE + '/me/activities/tracks/affiliated.json?oauth_token=' +
+            TOKEN + '&limit=10&filter=streamable'
+        )
+        if not r.ok:
+            r.raise_for_status()
+        self.data = r.json()
 
-def exit_():
-    sys.exit(1)
+    def fetch_tracks(self, data):
+        tracks = []
+        tracks.append({'next_href': data['next_href']})
 
-def listen_for_command():
-    io = raw_input()
-    parse_command(io)
+        for track in data['collection']:
+            tracks.append({
+                'id': track['origin']['id'],
+                'duration': track['origin']['duration'],
+                'genre': track['origin']['genre'],
+                'description': track['origin']['description'],
+                'downloadable': track['origin']['downloadable'],
+                'stream_url': track['origin']['stream_url'],
+                'permalink': track['origin']['permalink']
+            })
+        return tracks
 
-def parse_command(command):
-    commands[command]()
+    def play(self):
+        self.tracks = self.fetch_tracks(self.data)
+        print json.dumps(self.tracks[0], indent=2, sort_keys=True)
 
-commands = {
-    'help': help_,
-    'exit': exit_
-}
+class Controller():
+
+    def __init__(self):
+        self.playlist = Playlist()
+
+    def help_(self):
+        print 'This is the help page'
+
+    def exit_(self):
+        sys.exit(1)
+
+    def listen_for_command(self):
+        io = raw_input()
+        self.parse_command(io)
+
+    def parse_command(self, command):
+        commands = {
+            'help': self.help_,
+            'exit': self.exit_,
+            'play': self.playlist.play
+        }
+        commands[command]()
 
 def main():
+    controller = Controller()
     print 'Please enter a command. For a list of available commands type help'
     while True:
-        listen_for_command()
+        print '>>',
+        controller.listen_for_command()
 
 if __name__ == "__main__":
     main()
