7180c0717ed3ca086b337d095f938fa182faea00
Nicolas Spycher <nic.spycher@gmail.com>
2014-10-07 15:27:25 +0200

    SoundCloudr now fetches all the tracks up to the last listened track.

--- soundcloudr/__init__.py
+++ soundcloudr/__init__.py
@@ -6,96 +6,16 @@
 # To Public License, Version 2, as published by Sam Hocevar. See
 # http://www.wtfpl.net/ for more details.
 
-import os
-import re
 import sys
-import json
 
 from blessings import Terminal
-import requests
 
-TOKEN = '1-97489-6257437-80f8de20636a16b3'
-API_BASE = 'https://api.soundcloud.com'
-
-class Playlist():
-    def __init__(self):
-        r = requests.get(
-            API_BASE + '/me/activities/tracks/affiliated.json?oauth_token=' +
-            TOKEN + '&limit=3&filter=streamable'
-        )
-        if not r.ok:
-            r.raise_for_status()
-        self.data = r.json()
-        self.tracks = self.fetch_tracks(self.data)
-        self.current_track = None
-
-    def fetch_tracks(self, data):
-        tracks = []
-        tracks.append({'next_href': data['next_href']})
-
-        for track in data['collection']:
-            tracks.insert(0, {
-                'id': track['origin']['id'],
-                'title': track['origin']['title'],
-                'duration': track['origin']['duration'],
-                'genre': track['origin']['genre'],
-                'description': track['origin']['description'],
-                'downloadable': track['origin']['downloadable'],
-                'stream_url': track['origin']['stream_url'],
-                'permalink': track['origin']['permalink']
-            })
-        return tracks
-
-    def play(self):
-        #print json.dumps(self.tracks, indent=2, sort_keys=True)
-        if len(self.tracks) > 2:
-            self.current_track = self.tracks.pop(0)
-        else:
-            next_r = self.tracks[-1]
-            self.current_track = self.tracks.pop(0)
-            url = re.split('affiliated', next_r['next_href'])
-            request = (
-                    API_BASE +
-                    '/me/activities/tracks/affiliated.json?oauth_token=' +
-                    TOKEN + '&' + url[1][1:]
-                   )
-            r = requests.get(request)
-            data = r.json()
-            self.tracks = self.fetch_tracks(data)
-
-        '''stream_url = self.current_track['stream_url']
-        r = requests.get(stream_url + '?oauth_token=' +
-            TOKEN, stream=True)
-        for line in r.iter_lines():
-            if line:
-                print line'''
-
-    def next(self):
-        self.current_track = None
-        self.play()
-
-    def like(self):
-        return 0
-
-    def stop(self):
-        with open(
-                  os.path.join("/Users/Nic/Downloads/",
-                               ".last_listened.txt"), "w"
-                 ) as f:
-            f.write(str(self.current_track))
-        self.current_track = None
-
-    def get_current_track(self):
-        if self.current_track is not None:
-            print self.current_track['title']
-        else:
-            print ("Nothing is playing right now. " +
-                  "Go on and start playing some music!")
+import soundcloudr.playlist
 
 class Controller():
 
     def __init__(self):
-        self.playlist = Playlist()
+        self.playlist = soundcloudr.playlist.Playlist()
 
     def help_(self):
         help_page = '''
@@ -105,6 +25,7 @@ class Controller():
         play:       Play the newest tracks
         next:       Play the next track
         current:    Show the current track playing
+        show:       Show available tracks
         like:       Like the current track playing
         stop:       Stop playing music
         exit:       Close the program
@@ -130,7 +51,8 @@ class Controller():
             'current': self.playlist.get_current_track,
             'next': self.playlist.next,
             'like': self.playlist.like,
-            'stop': self.playlist.stop
+            'stop': self.playlist.stop,
+            'show': self.playlist.show
         }
         commands[command]()
 
